3dac725dab1530db97ebdd09d9b649843394a30d
Rohan singh <151426524+Rohan6023@users.noreply.github.com>
2024-11-24 13:52:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7318195718:AAH9ZCj0ETfuTTf55_wddyci2mWMUiy4hiU")
+BOT_TOKEN=("7818817713:AAFu_7aQ3RMbqxc7vlRxetWalmT0UtBVCzw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("NINJAGAMEROP")
+OWNER_USERNAME=("SASUKE")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1240179115")
+ADMIN_IDS=("5599402910")
 
 
 
